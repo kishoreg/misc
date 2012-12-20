9e41e61e7a28a2d818a455bb74279e79047a6bd1
Greg Brandt <gbrandt@linkedin.com>
2012-12-19 21:35:14 -0800

    added script that creates a tgz of svn changelist

---
+++ perl/svntar.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+# 
+# When executed from a subversion repository, given a changelist name, creates a
+# tgz archive of the changelist. Kind of like git stash
+#
+# @author Greg Brandt <gbrandt@linkedin.com>
+#
+use strict;
+use Getopt::Long;
+
+# Get args
+my $cl_name;
+my $out_file;
+GetOptions(
+        "cl=s" => \$cl_name,
+        "o=s" => \$out_file,
+);
+
+# Validate args
+die "usage: $0 -cl <cl-name> -o <out-file>" if !$cl_name || !$out_file;
+
+# Issue svn status command
+my $output = `svn st`;
+my @lines = split "\n", $output;
+
+# Read lines until we find the first file
+my $i = 0;
+while ($i++ < scalar(@lines)) {
+        if ($lines[$i] =~ /Changelist '$cl_name'/) {
+                $i++;   # Move to first file
+                last;   # And done 
+        }
+}
+
+# Check if we found it
+if ($i > scalar(@lines)) {
+        die "no changelist $cl_name!\n";
+}
+
+# Collect all the filenames
+my @files;
+while ($i < scalar(@lines) && $lines[$i] !~ /^\s*$/) {
+        my $f = $lines[$i++];
+        $f =~ s/[A-Z]\s* //;
+        push @files, $f;
+}
+
+# Tar them up
+my $f_str = join ' ', @files;
+print `tar cvzf $out_file $f_str`;
